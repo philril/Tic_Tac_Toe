50cc2f69af7d50ea25cf04d0e4f2cf1a98b0d340
Philip Riley & raorao <pair+philril.raorao@devbootcamp.com>
2014-12-18 20:07:08 -0800

    Revert "rao turns everything into a closure"
    This reverts commit a82438809738041be28e78d0e6a15e84da9bfd3c.

--- game.js
+++ game.js
@@ -1,43 +1,41 @@
-Game = function() {
-  var currentPiece = 'X'
-  var board = [[null, null, null],[null, null, null],[null, null, null]]
-  var done =  false
-  var winner =  null
+Game = {
+  currentPiece: 'X',
 
-  var checkForWinner = function(obj) {
+  board: [[null, null, null],[null, null, null],[null, null, null]],
+
+  addPiece: function(obj) {
+    var row = obj.row
+    var col = obj.col
+    if (this.board[row][col] === null){
+      this.board[row][col] = this.fetchCurrentPiece()
+    }
+    this.checkForWinner(obj)
+    return this.board[row][col]
+  },
+
+  checkForWinner: function(obj) {
     var row = obj.row
     var col = obj.col
 
     // checks current row
-    var value = board[row][col]
-    var check = board[row].every(function(cell) {
+    var value = this.board[row][col]
+    var check = this.board[row].every(function(cell) {
       return cell === value
     })
 
     if(check) {
-      done = true
-      winner = value
+      this.done = true
+      this.winner = value
     }
-  }
+  },
 
-  var fetchCurrentPiece = function() {
-    var piece = currentPiece
-    currentPiece = (currentPiece === 'X' ? 'O' : 'X')
+  fetchCurrentPiece: function() {
+    var piece = this.currentPiece
+    this.currentPiece = (this.currentPiece === 'X' ? 'O' : 'X')
     return piece
-  }
+  },
 
-  return {
-    isDone: function() { return done },
-    fetchWinner: function() { return winner },
-    board: board,
-    addPiece: function(obj) {
-      var row = obj.row
-      var col = obj.col
-      if (board[row][col] === null){
-        board[row][col] = fetchCurrentPiece()
-      }
-      checkForWinner(obj)
-      return board[row][col]
-    }
-  }
+  done: false,
+
+  winner: null
 }
--- tests.js
+++ tests.js
@@ -5,50 +5,51 @@ function assert(actual,expected) {
   if(expected === actual) {
     successes++
   } else {
-    console.error("expected ", expected, " got ", actual)
+    console.log("expected ", expected, " got ", actual)
     failures++
   }
 
 }
 
-game = Game()
-
 function resetBoard() {
-  game = Game()
+  Game.board = [[null, null, null],[null, null, null],[null, null, null]];
+  Game.currentPiece = 'X'
+  Game.done = false
+  Game.winner = null
 }
 printif = false
-assert(game.board instanceof Array, true)
-assert(game.board.length, 3)
-assert(game.board[0] instanceof Array, true)
-assert(game.board[0].length, 3)
-assert(game.board[0][0], null)
+assert(Game.board instanceof Array, true)
+assert(Game.board.length, 3)
+assert(Game.board[0] instanceof Array, true)
+assert(Game.board[0].length, 3)
+assert(Game.board[0][0], null)
 
-game.addPiece({row: 1, col: 2})
-assert(game.board[1][2],'X')
+Game.addPiece({row: 1, col: 2})
+assert(Game.board[1][2],'X')
 
-game.addPiece({row:1, col:0})
-assert(game.board[1][0], 'O')
+Game.addPiece({row:1, col:0})
+assert(Game.board[1][0], 'O')
 
-game.addPiece({row:1, col:0})
-assert(game.board[1][0], 'O')
+Game.addPiece({row:1, col:0})
+assert(Game.board[1][0], 'O')
 
-assert( game.addPiece({row:1, col:1}), 'X')
-
-resetBoard()
-game.addPiece({row:0, col:0})
-game.addPiece({row:1, col:1})
-game.addPiece({row:0, col:1})
-game.addPiece({row:1, col:2})
-game.addPiece({row:0, col:2})
-assert(game.isDone(), true)
-assert(game.fetchWinner(), 'X')
+assert( Game.addPiece({row:1, col:1}), 'X')
 
 resetBoard()
-game.addPiece({row:0, col:0})
-game.addPiece({row:0, col:1})
-game.addPiece({row:0, col:2})
-assert(game.isDone(), false)
-assert(game.fetchWinner(), null)
+Game.addPiece({row:0, col:0})
+Game.addPiece({row:1, col:1})
+Game.addPiece({row:0, col:1})
+Game.addPiece({row:1, col:2})
+Game.addPiece({row:0, col:2})
+assert(Game.done, true)
+assert(Game.winner, 'X')
+
+resetBoard()
+Game.addPiece({row:0, col:0})
+Game.addPiece({row:0, col:1})
+Game.addPiece({row:0, col:2})
+assert(Game.done, false)
+assert(Game.winner, null)
 
 console.log(successes + " tests passed.")
 console.log(failures + " tests failed.")
