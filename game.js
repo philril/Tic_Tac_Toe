81e2d69d1e06c8011e4cb8bfd7d26f7797e2e081
Philip Riley & raorao <pair+philril.raorao@devbootcamp.com>
2014-12-18 19:21:31 -0800

    addPiece logic now functional
    TODO: does not handle double-clicking

--- game.js
+++ game.js
@@ -1,4 +1,17 @@
 Game = {
-  board: [[null, null, null],[null, null, null],[null, null, null]]
+  currentPiece: 'X',
 
+  board: [[null, null, null],[null, null, null],[null, null, null]],
+
+  addPiece: function(obj) {
+    var row = obj.row
+    var col = obj.col
+    this.board[row][col] = this.fetchCurrentPiece()
+  },
+
+  fetchCurrentPiece: function() {
+    var piece = this.currentPiece
+    this.currentPiece = (this.currentPiece === 'X' ? 'O' : 'X')
+    return piece
+  }
 }
--- tests.js
+++ tests.js
@@ -17,7 +17,11 @@ assert(Game.board[0] instanceof Array, true)
 assert(Game.board[0].length, 3)
 assert(Game.board[0][0], null)
 
+Game.addPiece({row: 1, col: 2})
+assert(Game.board[1][2],'X')
 
+Game.addPiece({row:1, col:0})
+assert(Game.board[1][0], 'O')
 
 console.log(successes + " tests passed.")
 console.log(failures + " tests failed.")
